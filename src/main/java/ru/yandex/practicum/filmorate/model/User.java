218744748030edd283a0e49258cd9ac37f61949f
n.kozlov <nikitacauselove@gmail.com>
2023-04-13 03:29:23 +0300

    feat: добавить валидацию для методов HTTP

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,15 +1,21 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
 public class User {
     private int id;
+    @NotNull
     private final String email;
+    @NotNull
     private final String login;
     private final String name;
+    @NotNull
     private final LocalDate birthday;
 
     public User(final String email, final String login, final LocalDate birthday) {
@@ -19,15 +25,15 @@ public class User {
         this.birthday = birthday;
     }
 
-    public boolean isValid() throws ValidationException {
+    public boolean isValid() throws ResponseStatusException {
         if (!email.contains("@") || email.contains(" ")) {
-            throw new ValidationException("Электронная почта не может быть пустой и должна содержать символ @.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Электронная почта не может быть пустой и должна содержать символ @.");
         }
         if (login.contains(" ")) {
-            throw new ValidationException("Логин не может быть пустым и содержать пробелы.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Логин не может быть пустым и содержать пробелы.");
         }
         if (birthday.isAfter(LocalDate.now())) {
-            throw new ValidationException("Дата рождения не может быть в будущем.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Дата рождения не может быть в будущем.");
         }
         return true;
     }
