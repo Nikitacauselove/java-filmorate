1d47c95bfe76b53e66ccbbd602c4f8bfaa5119b0
n.kozlov <nikitacauselove@gmail.com>
2023-04-21 14:26:51 +0300

    fix: исправить использование конструктора класса User

--- src/test/java/ru/yandex/practicum/filmorate/model/UserTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/model/UserTest.java
@@ -11,21 +11,21 @@ class UserTest {
 
     @Test
     public void invalidEmail() {
-        User user = new User("mail.ru", "", "dolore ullamco", LocalDate.parse("1980-08-20"));
+        User user = new User("mail.ru", "dolore ullamco", "", LocalDate.parse("1980-08-20"));
 
         assertThrows(ResponseStatusException.class, user::isValid);
     }
 
     @Test
     public void invalidLogin() {
-        User user = new User("yandex@mail.ru", null, "dolore ullamco", LocalDate.parse("2446-08-20"));
+        User user = new User("yandex@mail.ru", "dolore ullamco", null, LocalDate.parse("2446-08-20"));
 
         assertThrows(ResponseStatusException.class, user::isValid);
     }
 
     @Test
     public void invalidBirthday() {
-        User user = new User("test@mail.ru", "", "dolore", LocalDate.parse("2446-08-20"));
+        User user = new User("test@mail.ru", "dolore", "", LocalDate.parse("2446-08-20"));
 
         assertThrows(ResponseStatusException.class, user::isValid);
     }
